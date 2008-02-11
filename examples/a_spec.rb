f2f81f7c735eab11245beec842fe9da94ad290ee
Caspar Florian Ebeling <florian.ebeling@gmail.com>
2008-02-11 21:42:10 +0000

    One more spec example.

--- examples/a_spec.rb
+++ examples/a_spec.rb
@@ -5,5 +5,8 @@ describe "A failing spec" do
   it "recurses 5 times and then fails" do
     lambda { recurse(5) }.should_not raise_error
   end
-
+  
+  it "this is a simple failure" do
+    "this".should == "that"
+  end
 end

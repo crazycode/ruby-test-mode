e6ee60f834607d8372e6accc75376c12675ef0dd
Caspar Florian Ebeling <florian.ebeling@gmail.com>
2008-08-22 21:31:50 +0200

    fix the example test cases

--- examples/a_spec.rb
+++ examples/a_spec.rb
@@ -1,9 +1,10 @@
+$:.unshift(File.dirname(__FILE__))
 require "failure"
 
 describe "A failing spec" do
 
   it "recurses 5 times and then fails" do
-    lambda { recurse(5) }.should_not raise_error
+    recurse(5)
   end
   
   it "this is a simple failure" do
